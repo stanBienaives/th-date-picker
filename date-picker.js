8a1d19cbe43547bee3c70bad378b6722875243b7
Stan.bienaives@gmail.com <stan.bienaives@gmail.com>
2017-09-17 09:49:30 +0200

    pick hour logic

--- date-picker.js
+++ date-picker.js
@@ -1,22 +1,12 @@
 var WEEKDAYS = ['D','L','M','M','J','V','S'];
 
-//var datePicker = function(params) {
-  //// min booking frame
-  //// max booking frame
-  //// opening time
-  ////
-  //this.selected = [];
-  //this.currentDate;
-  //this.displayWeekdays();
-  //this.displayDays();
-//}
-
 var State = function State() {
   this.selected = [];
   this.availableDates = [];
 
-
   var today = new Date();
+
+  this.currentDate = today;
 
   for (var i=0; i<90;i++) {
     this.availableDates.push(today.addDay(i));
@@ -30,16 +20,36 @@ State.prototype.isDateSelected = function (date) {
   return !!selected;
 }
 
+State.prototype.isHourSelected = function (date) {
+  var selected =  this.selected.find(function (d) {
+    return d.toDateString() + d.getHours() == date.toDateString() + date.getHours();
+  })
+  return !!selected;
+}
+
+State.prototype.isCurrentDate = function (date) {
+  return this.currentDate.toDateString() == date.toDateString();
+}
+
 
 //ACTION RECORD
 var Actions = {
   "TOOGLE_DATE": function (state, date) {
-    var position = state.selected.indexOf(date);
+    var position = state.selected.map(function (d) {
+      return d.getDayAndHours();
+    }).indexOf(date.getDayAndHours());
+
     if( position === -1 ) {
       state.selected.push(date);
     } else {
       state.selected.splice(position, 1);
     }
+    console.log(state.selected);
+    state.currentDate = date;
+    this.render(state);
+  },
+  "SET_CURRENT_DATE": function (state, date) {
+    state.currentDate = date;
     this.render(state);
   },
 }
@@ -59,7 +69,8 @@ datePickerComponent.prototype.render = function (state, oldState) {
 
 
 //WEEKDAY COMPONENT
-var weekdaysComponent = function () {}
+var weekdaysComponent = function () {
+}
 
 weekdaysComponent.prototype.render =function(state, oldState) {
   var wrapper = document.getElementById('cal-wrapper');
@@ -71,16 +82,46 @@ weekdaysComponent.prototype.render =function(state, oldState) {
     weekDay.innerText = WEEKDAYS[date.getDay()];
   });
 
-
   wrapper.appendChild(weekdaysContainer);
 
 }
 
+// HOURS COMPONENT
+var hoursComponent = function (state) {
+  var wrapper = document.getElementById('cal-wrapper');
+  this.container =  document.createElement('div');
+  this.container.id = 'cal-hours';
+  this.container.className = 'cal-hours';
+  wrapper.appendChild(this.container);
+};
 
+hoursComponent.prototype.render = function(state, oldState) {
+  var wrapper = document.getElementById('cal-wrapper');
+  var hoursContainer = document.getElementById('cal-hours');
+
+  this.container.innerHTML = '';
+
+  for(var i=6;i < 22;i++) {
+    var d = new Date(state.currentDate);
+    d.setHours(i);
+    var hour = document.createElement('div');
+    hour.className = 'cal-hour';
+    if(state.isHourSelected(d)) {
+      hour.className += ' selected';
+    }
+    hour.innerHTML = '<span>' + d.getHours() + '</span>';
+    hour.addEventListener("click", Actions["TOOGLE_DATE"].bind(this, state, d));
+    this.container.appendChild(hour);
+  }
+
+  //wrapper.appendChild(hoursContainer);
+
+}
 
 //DAY COMPONENTS
-
-var daysComponent = function () {}
+var daysComponent = function () {
+  this.hoursView = new hoursComponent();
+}
 
 daysComponent.prototype.render = function (state, oldState) {
   var wrapper = document.getElementById('cal-wrapper');
@@ -97,28 +138,19 @@ daysComponent.prototype.render = function (state, oldState) {
     day.className = 'cal-day';
     day.innerHTML =  '<span>' + date.getDate(); + '</span>';
 
-    if (state.isDateSelected(date)) {
+    if (state.isDateSelected(date) || state.isCurrentDate(date)) {
       day.className += ' selected';
     }
-    day.addEventListener("click", Actions["TOOGLE_DATE"].bind(this, state, date));
-    //if(date == 4)
-      //day.className += ' selected';
-    //day.addEventListener("click", function() {
-      //if (this.className.indexOf('selected') === -1)
-        //this.className += ' selected';
-      //else
-        //this.className = this.className.replace('selected','');
-    //});
+    day.addEventListener("click", Actions["SET_CURRENT_DATE"].bind(this, state, date));
 
   }.bind(this));
 
   wrapper.appendChild(daysContainer);
+  this.hoursView.render(state, oldState);
 
 }
 
 // HELPER
-
-
 var appendColumnsHelper = function(parent, array, buildFunction) {
   for(var i=0;i < array.length;i++) {
     var elem = document.createElement('div');
@@ -131,6 +163,11 @@ Date.prototype.addDay = function (days) {
   var dat = new Date(this.valueOf());
   dat.setDate(dat.getDate() + days);
   return dat;
+}
+
+Date.prototype.getDayAndHours = function () {
+  var dat = new Date(this.valueOf());
+  return dat.getYear() + '-' + dat.getMonth() + '-' + dat.getDate() + ' ' + dat.getHours();
 }
 
 var initialState = new State();
