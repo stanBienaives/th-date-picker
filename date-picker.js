55b0e4703c0d89d26419eef5dcb46cdcf0eb5114
Stan.bienaives@gmail.com <stan.bienaives@gmail.com>
2017-09-24 19:29:49 +0200

    add morning

--- date-picker.js
+++ date-picker.js
@@ -118,13 +118,15 @@ var datePickerComponent = function () {
   this.navigatorView = new navigatorComponent();
   this.weekdaysView = new weekdaysComponent();
   this.daysView = new daysComponent();
+  var initialState = new State();
+  this.render(initialState);
 }
 
 
 datePickerComponent.prototype.render = function (state, oldState) {
   this.navigatorView.render(state, oldState, this);
   this.weekdaysView.render(state, oldState);
-  this.daysView.render(state, oldState);
+  this.daysView.render(state, oldState, this);
 }
 
 
@@ -199,6 +201,12 @@ hoursComponent.prototype.render = function(state, oldState) {
   for(var i=0; i < 3; i++) {
     var silo = document.createElement('div');
     silo.className = 'cal-hour-silo';
+    if(i==0)
+      silo.innerHTML = '<span> MATINEE </span>';
+    if(i==1)
+      silo.innerHTML = '<span> JOURNEE </span>';
+    if(i==2)
+      silo.innerHTML = '<span> SOIREE </span>';
     this.container.appendChild(silo);
     this.silos.push(silo);
   }
@@ -247,10 +255,32 @@ var daysComponent = function () {
   this.container.id = 'cal-days';
   this.container.className = 'cal-days'
   wrapper.appendChild(this.container);
+
   this.hoursView = new hoursComponent();
 }
 
-daysComponent.prototype.render = function (state, oldState) {
+daysComponent.prototype.render = function (state, oldState, parent) {
+
+  if( !this.rendered) {
+    swipedetect(this.container, function (dir) {
+      console.log('direction', dir);
+      if( dir == 'right') {
+        console.log('previous panel');
+        Actions['PREVIOUS_PANEL'].bind(parent)(currentState());
+      }
+
+      if (dir == 'left') {
+        Actions['NEXT_PANEL'].bind(parent)(currentState());
+      }
+
+    }.bind(this));
+  }
+    //
+  function currentState() {
+    return state;
+  }
+
+  this.rendered = true;
   this.container.innerHTML = '';
 
   var start = state.cursor * 7 * state.displayedLines;
@@ -321,8 +351,54 @@ Date.prototype.getDayAndHours = function () {
   return dat.getYear() + '-' + dat.getMonth() + '-' + dat.getDate() + ' ' + dat.getHours();
 }
 
-var initialState = new State();
-var datePickerView = new datePickerComponent();
-datePickerView.render(initialState);
+//var datePickerView = new datePickerComponent();
 
 
+
+// Shameless copy/paste
+function swipedetect(el, callback){
+
+    var touchsurface = el,
+    swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 15, //required min distance traveled to be considered swipe
+    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 3000, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+    handleswipe = callback || function(swipedir){}
+
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        //e.preventDefault()
+    }, false)
+
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault() // prevent scrolling when inside DIV
+    }, false)
+
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if (elapsedTime <= allowedTime){ // first condition for awipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        handleswipe(swipedir)
+        //e.preventDefault()
+    }, false)
+}
